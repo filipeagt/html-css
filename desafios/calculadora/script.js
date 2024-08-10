58853d93e7dc6a9ad0408861530ebfb4e8a282df
Filipe Almeida <filipeagt.92@gmail.com>
2024-08-10 18:56:01 -0300

    Atualização calculadora
    Implementadas as operações * e /
    alterado o estilo

--- desafios/calculadora/script.js
+++ desafios/calculadora/script.js
@@ -7,40 +7,53 @@ function calc(tecla) {
         case 'AC':
             acumulador = 0
             display.innerText = '0'
+            operando = false
+            operador = ''
             break
         case 'sqrt':
             acumulador = Math.sqrt(Number(display.innerText))
-            display.innerText = acumulador//.toPrecision(9)
-            break
+            display.innerText = acumulador.toLocaleString('en', {maximumSignificantDigits: '8'})
+            tecla = '='
+            
         case '%':
             break
-        case '/':
-            break
-        case '*':
-            break
         case '+':
-            acumulador += Number(display.innerText)
-            operador = '+'
+            /*acumulador += Number(display.innerText.replace(',',''))
+            display.innerText = acumulador.toLocaleString('en', {maximumSignificantDigits: '8'})
+            operador = tecla
             operando = true
-            break
+            break*/
+        case '/':
+        case '*':
         case '-':
-            acumulador += Number(display.innerText)
-            operador = '-'
+            acumulador = Number(display.innerText.replace(',',''))
+            display.innerText = acumulador.toLocaleString('en', {maximumSignificantDigits: '8'})
+            operador = tecla
             operando = true
+            //tecla = '='
             break
+        
         case '=':
             switch(operador){
                 case '+':
                     acumulador += Number(display.innerText)
-                    operador = ''
                     break
                 case '-':
                     acumulador -= Number(display.innerText)
-                    operador = ''
                     break
+                case '*':
+                    acumulador *= Number(display.innerText)
+                    break
+                case '/':
+                    acumulador /= Number(display.innerText)
+                    break
+                default:
+                    acumulador = Number(display.innerText)
             }
-            display.innerText = acumulador
+            display.innerText = acumulador.toLocaleString('en', {maximumSignificantDigits: '8'})
             acumulador = 0
+            operando = true
+            operador = ''
             break
         default:
             if(display.innerText=='0' || operando) {
