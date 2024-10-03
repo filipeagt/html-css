d47a4336accead4fad398cb953c3d87bbc13b190
Filipe Almeida <filipeagt.92@gmail.com>
2024-10-03 14:09:54 -0300

    Update script.js

--- desafios/relogio-digital/js/script.js
+++ desafios/relogio-digital/js/script.js
@@ -1,15 +1,16 @@
 function relogio() {
     
     let display = document.getElementById('relogio')
+    let separador = ':'
     
     setInterval(() => {
         let tempo = new Date()
         let segundos = tempo.getSeconds()
         let minutos = tempo.getMinutes()
         let horas = tempo.getHours()
+        separador = separador == ':'? ' ' : ':'
 
-
-        display.innerText = `${horas<10?'0'+horas:horas}:${minutos<10?'0'+minutos:minutos}:${segundos<10?'0'+segundos:segundos}`
-    }, 1000);
+        display.innerText = `${horas<10?'0'+horas:horas}${separador}${minutos<10?'0'+minutos:minutos}${separador}${segundos<10?'0'+segundos:segundos}`
+    }, 500);
 
 }
